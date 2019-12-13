53072c464fcc0bb62b99d1c340fa143cfff75f76
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2019-12-12 19:36:28 -0700

    first commit

---
+++ number8.js
@@ -0,0 +1,36 @@
+let firstName = "Jim";
+
+console.log (firstName)
+/*Javascript is a dynamically typed language. As you assign a variable the 
+interpreter infers what data type you're intending instead of manually stating it's
+a data type such as a string*/
+/* This illustrates the sring datatype in Javascript*/
+
+let age = 26;
+console.log (age)
+
+let price = 6.50;
+console.log (price)
+
+/* Javascript interprets numbers whether integers or floating decimals points
+without the developer assigning them*/
+
+// Booleans
+
+let vote = true;
+console.log (vote)
+
+let noVote = false;
+console.log (noVote)
+
+// Undefined
+
+let hasNoValue;
+console.log (hasNoValue)
+// Javascript is saying to us the we have not yet provided a value for the expression
+
+/* Its best not to leave an empty value undefined. Giving it a null (Not a zero value)
+is the best solution*/
+
+let calculatedBirthDate = null;
+console.log (calculatedBirthDate)
